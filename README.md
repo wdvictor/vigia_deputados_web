accbd7fa7ea4d46afcbcde55b6b47e1b990e38ec
victor.hugo <victorhpantunes@gmail.com>
2023-07-18 14:22:31 -0300

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vigia Deputados
+
+## https://vigia-deputados.web.app/
+
+Site para coletar dados sobre deputados
+
+![](/images/Screenshot%20from%202023-07-18%2014-16-20.png)
+
+![](/images/Screenshot%20from%202023-07-18%2014-16-35.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { SimpleGrid, VStack, Box } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
-import { spanishGreen } from "./custom-theme";
+
 import { useEffect, useState } from "react";
 import useDeputados from "./hooks/UseDeputados";
 import DeputadoCard from "./components/home-page-components/DeputadoCard";
 import DeputadoContainer from "./components/home-page-components/DeputadoContainer";
 import DeputadosSkeleton from "./components/home-page-components/DeputadosSkeleton";
 import Paginacao from "./components/home-page-components/Paginacao";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import firebaseConfig from "../firebase-config.json";
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function App() {
   const [pagina, setPagina] = useState<number>(
@@ -28,12 +22,12 @@ function App() {
   if (error) return <div className="error-message text-danger">{error}</div>;
   return (
     <Box w="100%" m="0px" p="0px">
-      <VStack backgroundColor={`${spanishGreen}`} h="100%">
+      <VStack h="100%">
         <NavBar></NavBar>
         <SimpleGrid
           padding="10px"
           columns={{ sm: 1, md: 2, lg: 3, xl: 3, "2xl": 3 }}
-          spacing="10"
+          spacing="5"
         >
           {isLoading &&
             skeletons.map((s) => (
