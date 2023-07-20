515c425699dfed38579fadd8c00202e290393c99
victor.hugo <victorhpantunes@gmail.com>
2023-07-20 12:37:48 -0300

    Creating orgãos timeline and changing design of the cards

--- src/components/deputado-perfil-components/Orgaos.tsx
+++ src/components/deputado-perfil-components/Orgaos.tsx
@@ -1,5 +1,6 @@
 import {
   Badge,
+  Box,
   Card,
   CardBody,
   CardFooter,
@@ -7,21 +8,15 @@ import {
   HStack,
   Spacer,
   Text,
+  VStack,
   Wrap,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import useDeputadoOrgaos from "../../hooks/useDeputadoOrgaos";
+import useDeputadoOrgaos, { Dado } from "../../hooks/useDeputadoOrgaos";
 import { secondaryColor } from "../../custom-theme";
 
 const Orgaos = ({ deputadoID }: { deputadoID: number }) => {
   const { data } = useDeputadoOrgaos(deputadoID);
-  const isLargeScreen = useBreakpointValue({
-    base: false,
-    sm: false,
-    md: false,
-    lg: true,
-    xl: true,
-  });
 
   function formatDate(date: string | undefined): string {
     if (date) {
@@ -31,41 +26,82 @@ const Orgaos = ({ deputadoID }: { deputadoID: number }) => {
     }
     return "";
   }
+
+  let dateLabelsSet = new Set();
+  function addLabel(date: string): string {
+    let dateF = formatDate(date);
+    if (dateLabelsSet.has(dateF)) {
+      return "";
+    } else {
+      dateLabelsSet.add(dateF);
+      return dateF;
+    }
+  }
   return (
-    <Wrap spacing="5">
-      {data?.dados.map((orgao) => (
-        <Card
-          key={`${orgao.idOrgao}${orgao.codTitulo}`}
-          backgroundColor={secondaryColor}
-          maxW={isLargeScreen ? "45%" : "100%"}
-        >
-          <CardHeader color="white">
-            <HStack>
-              <Text m="0px" fontFamily="inter-extrabold">
-                {orgao.siglaOrgao}
+    <VStack
+      w="100vw"
+      p="20px"
+      align="start"
+      maxH="100vh"
+      overflow="scroll"
+      spacing="0"
+    >
+      {data?.dados.map((orgao, index) => (
+        <Box w="100%" borderLeft="2px solid black" pl="10px" key={index}>
+          <HStack align="flex-start">
+            <Box w="70px">
+              <Text m="0" fontFamily="inter-bold" color="gray">
+                {addLabel(orgao.dataInicio)}
               </Text>
-              <Spacer />
-              <Badge p="2px 5px 2px 5px">{orgao.titulo}</Badge>
-            </HStack>
-          </CardHeader>
-          <CardBody color="white" fontFamily="inter-medium">
-            {orgao.nomeOrgao.toLowerCase()}
-          </CardBody>
-          <CardFooter color="white">
-            <HStack w="100%">
-              <Text m="0px" fontFamily="inter-extrabold">
-                Início:
-              </Text>
-              <Text m="0px"> {formatDate(orgao.dataInicio)}</Text>
-              <Spacer />
-              {orgao.dataFim && (
-                <Text m="0px">Fim: {formatDate(orgao.dataFim)}</Text>
-              )}
-            </HStack>
-          </CardFooter>
-        </Card>
+            </Box>
+
+            <Card
+              key={`${orgao.idOrgao}${orgao.codTitulo}`}
+              border={`2px solid ${secondaryColor}`}
+              maxW={"45%"}
+              mt="30px"
+              mb="30px"
+            >
+              <CardHeader color="white">
+                <HStack>
+                  <Text m="0px" fontFamily="inter-extrabold" color="black">
+                    {orgao.siglaOrgao}
+                  </Text>
+                  <Spacer />
+                  <Badge
+                    p="2px 15px 2px 15px"
+                    bg={secondaryColor}
+                    borderRadius="5px"
+                    color="white"
+                  >
+                    {orgao.titulo}
+                  </Badge>
+                </HStack>
+              </CardHeader>
+              <CardBody color="black" fontFamily="inter-medium">
+                {orgao.nomeOrgao.toLowerCase()}
+              </CardBody>
+              <CardFooter color="white">
+                <HStack w="100%">
+                  <Text m="0px" fontFamily="inter-medium" color="black">
+                    Início:
+                  </Text>
+                  <Text m="0px" color="black" fontFamily="inter-medium">
+                    {formatDate(orgao.dataInicio)}
+                  </Text>
+                  <Spacer />
+                  {orgao.dataFim && (
+                    <Text m="0px" color="black" fontFamily="inter-medium">
+                      Fim: {formatDate(orgao.dataFim)}
+                    </Text>
+                  )}
+                </HStack>
+              </CardFooter>
+            </Card>
+          </HStack>
+        </Box>
       ))}
-    </Wrap>
+    </VStack>
   );
 };
 
